257ddebdddd5693a028d71d1dd9f4aa4a0aad56b
MaltheAN <maltheasmusn@gmail.com>
2022-12-13 11:56:35 +0100

    amp rabi ex

--- dataanalyzer/fitter/models/__init__.py
+++ dataanalyzer/fitter/models/__init__.py
@@ -1,10 +1,1 @@
-from dataanalyzer.fitter.models.fitter_models import (
-    LinearModel,
-    PolynomialModel,
-    GaussianModel,
-    LorentzianModel,
-    SplitLorentzianModel,
-    LorentzianPolynomialModel,
-    GaussianConstantModel,
-)
-
+from dataanalyzer.fitter.models.fitter_models import *
--- setup.py
+++ setup.py
@@ -9,8 +9,8 @@ import versioneer
 
 readme_file_path = Path(__file__).absolute().parent / "README.md"
 
-required_packages = ['opencensus-ext-azure']
-package_data = {"dataanalyzer": ["conf/telemetry.ini"] }
+required_packages = ["opencensus-ext-azure"]
+package_data = {"dataanalyzer": ["conf/telemetry.ini"]}
 
 
 setup(
@@ -19,7 +19,7 @@ setup(
     cmdclass=versioneer.get_cmdclass(),
     python_requires=">=3.9",
     install_requires=required_packages,
-    author= "Malthe Asums Marciniak Nielsen",
+    author="Malthe Asums Marciniak Nielsen",
     author_email="vpq602@alumni.ku.dk",
     description="package_description",
     long_description=readme_file_path.open().read(),

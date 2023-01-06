79eb0b1f5c9a0f9e05fd41adc08f6584fda4eb2c
MaltheAN <maltheasmusn@gmail.com>
2023-01-06 14:08:43 +0100

    Removed old sandbox code

--- sandbox/unit_fixing.py
+++
@@ -1,9 +0,0 @@
-from dataanalyzer import Plotter, Fitter, fitmodels, load_labber_file
-import os
-
-func = fitmodels.DampedOscillationModel()
-func.units("x", "y")
-func.units(1, 1)
-func.param_names
-func.symbols(amplitude="Test")
-func.symbols(overwrite=True)

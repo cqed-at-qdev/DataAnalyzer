01802bc1ce7c1008a1f16e461cbb2100620b673f
MaltheAN <maltheasmusn@gmail.com>
2023-01-23 07:45:25 +0100

    pdoc documentation

--- dataanalyzer/utilities/__init__.py
+++ dataanalyzer/utilities/__init__.py
@@ -2,11 +2,6 @@ from dataanalyzer.utilities.data_loader import (
     load_json_file,
     load_labber_file,
 )
-from dataanalyzer.utilities.convert_json_to_labber import (
-    make_labber_dict,
-    make_inital_Labber_file,
-    json2Labber,
-)
 from dataanalyzer.utilities.valueclass import Valueclass
 from dataanalyzer.utilities.utilities import (
     convert_array_with_unit,

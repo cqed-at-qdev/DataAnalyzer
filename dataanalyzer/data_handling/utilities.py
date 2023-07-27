90f1c6050b7c9538097714471d0d5515682adc24
JacobHast <jacobhastrup@gmail.com>
2023-07-27 09:57:15 +0200

    Added string_to_attrs() and cleaned attrs_to_string()

--- dataanalyzer/data_handling/utilities.py
+++ dataanalyzer/data_handling/utilities.py
@@ -1,34 +1,87 @@
 import xarray as xr
+from typing import Protocol, Callable, Any
+
+
+####################################################################################################
+### handling of non-compatible types in attributes #################################################
+####################################################################################################
+
+
+# Typing class specifying that the object has an attrs attribute of type dict[str, any]
+class HasAttrs(Protocol):
+    attrs: dict[str, Any]
 
 
 def attrs_to_string(ds: xr.Dataset, attr_type: type) -> xr.Dataset:
-    """Replaces attrs of type attr_type with a string representation to allow saving to netcdf.
-    For example, None type cannot be saved to netcdf, but str(None) can."""
+    """Replaces attrs of type attr_type with a string representation to allow saving to netcdf. Also adds a f"{key}__type" attribute to the attrs dict to allow restoring the original type.
+    Intented for simple datatypes. For example, None type cannot be saved to netcdf, but str(None) can.
+    Returns a copy of the dataset with the changed attributes"""
+
+    def _replace_in_data(data: HasAttrs) -> None:
+        to_be_added = {}
+        for attr_key, attr_value in data.attrs.items():
+            if type(attr_value) == attr_type:
+                data.attrs[attr_key] = str(attr_value)
+                to_be_added[f"{attr_key}__type"] = str(type(attr_value))
+        data.attrs.update(to_be_added)
+
     ds = ds.copy()
 
     # Replace attributes of coordinates
     for coord_key in ds.coords:
-        for attr_key, attr_value in ds.coords[coord_key].attrs.items():
-            if type(attr_value) == attr_type:
-                ds.coords[coord_key].attrs[attr_key] = str(attr_value)
+        _replace_in_data(ds.coords[coord_key])
 
     # Replace attributes of dataarrays
     for da_key in ds:
-        for attr_key, attr_value in ds[da_key].attrs.items():
-            if type(attr_value) == attr_type:
-                ds[da_key].attrs[attr_key] = str(attr_value)
+        _replace_in_data(ds[da_key])
 
     # Replace attributes of dimensions
     for dim_key in ds.dims:
-        for attr_key, attr_value in ds[dim_key].attrs.items():
-            if type(attr_value) == attr_type:
-                ds[dim_key].attrs[attr_key] = str(attr_value)
+        _replace_in_data(ds[dim_key])
 
     # Replace global attributes of dataset
-    for attr_key, attr_value in ds.attrs.items():
-        if type(attr_value) == attr_type:
-            ds.attrs[attr_key] = str(attr_value)
+    _replace_in_data(ds)
+
     return ds
+
+
+def string_to_attrs(
+    ds: xr.Dataset, type_string: str, str_to_type_fnc: Callable[[str], Any]
+) -> xr.Dataset:
+    """Replaces attrs of type type_string with the result of str_to_type_fnc.
+    Used to restore attributes that were replaced by attrs_to_string."""
+
+    def _restore_attr(data: HasAttrs) -> None:
+        to_be_deleted = []
+        for attr_key, attr_value in data.attrs.items():
+            if attr_key.endswith("__type") and attr_value == type_string:
+                attr_main_key = attr_key[:-6]
+                data.attrs[attr_main_key] = str_to_type_fnc(data.attrs[attr_main_key])
+                to_be_deleted.append(attr_key)
+        for key in to_be_deleted:
+            del data.attrs[key]
+
+    # Restore attributes of coordinates
+    for coord_key in ds.coords:
+        _restore_attr(ds.coords[coord_key])
+
+    # Restore attributes of dataarrays
+    for da_key in ds:
+        _restore_attr(ds[da_key])
+
+    # Restore attributes of dimensions
+    for dim_key in ds.dims:
+        _restore_attr(ds[dim_key])
+
+    # Restore global attributes of dataset
+    _restore_attr(ds)
+
+    return ds
+
+
+####################################################################################################
+### Version checking ###############################################################################
+####################################################################################################
 
 
 def is_version_update_compatible(
@@ -50,7 +103,7 @@ def is_version_update_compatible(
 
 
 ####################################################################################################
-### unumpy handling functions #######################################################################
+### unumpy handling functions ######################################################################
 ####################################################################################################
 
 

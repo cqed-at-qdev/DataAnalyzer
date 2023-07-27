801d5ae4e28840ece0c6514908264b9da6e108ad
JacobHast <jacobhastrup@gmail.com>
2023-07-27 11:21:09 +0200

    changed docstring

--- dataanalyzer/data_handling/io_versions/io_0_0_2.py
+++ dataanalyzer/data_handling/io_versions/io_0_0_2.py
@@ -3,13 +3,13 @@ from dataanalyzer.data_handling.utilities import attrs_to_string, string_to_attr
 
 
 def save(ds: xr.Dataset, path: str) -> None:
-    """Saves dataset as netcdf to path, version 0.0.2"""
+    """Saves dataset as netcdf to path, SQuID lab data_version 0.0.2"""
     ds = attrs_to_string(ds, type(None))
     ds.to_netcdf(path)
 
 
 def format_loaded_dataset(ds: xr.Dataset) -> xr.Dataset:
-    """Formats the loaded dataset, version 0.0.2
-    No formats are needed in this version, so this function just returns the dataset"""
+    """Formats the loaded dataset, SQuID lab data_version 0.0.2 \n
+    None strings are restored to None type"""
     ds = string_to_attrs(ds, str(type(None)), lambda s: None)
     return ds

59978476b3896d95b74473d8f88f49239468da90
JacobHast <jacobhastrup@gmail.com>
2023-09-21 11:32:05 +0200

    Changed attrs formatting code structure

--- dataanalyzer/data_handling/io_versions/io_0_0_2.py
+++ dataanalyzer/data_handling/io_versions/io_0_0_2.py
@@ -1,15 +1,15 @@
 import xarray as xr
-from dataanalyzer.data_handling.utilities import attrs_to_string, string_to_attrs
+from dataanalyzer.data_handling.utilities import format_none_type, restore_none_type
 
 
 def save(ds: xr.Dataset, path: str) -> None:
     """Saves dataset as netcdf to path, SQuID lab data_version 0.0.2"""
-    ds = attrs_to_string(ds, type(None))
+    ds = format_none_type(ds)
     ds.to_netcdf(path)
 
 
 def format_loaded_dataset(ds: xr.Dataset) -> xr.Dataset:
     """Formats the loaded dataset, SQuID lab data_version 0.0.2 \n
     None strings are restored to None type"""
-    ds = string_to_attrs(ds, str(type(None)), lambda s: None)
+    ds = restore_none_type(ds)
     return ds
--- dataanalyzer/data_handling/io_versions/io_0_0_3.py
+++ dataanalyzer/data_handling/io_versions/io_0_0_3.py
@@ -1,30 +1,36 @@
 import xarray as xr
 from dataanalyzer.data_handling.utilities import (
-    attrs_to_string,
     seperate_from_unumpy,
     combine_to_unumpy,
-    string_to_attrs,
     flatten_attrs,
     unflatten_attrs,
+    format_none_type,
+    restore_none_type,
+    format_ufloat_type,
+    restore_ufloat_type,
 )
 
 
 def save(ds: xr.Dataset, path: str) -> None:
     """Saves dataset as netcdf to path, version 0.0.3"""
     ds = flatten_attrs(ds)
-    ds = attrs_to_string(ds, type(None))
+    ds = format_none_type(ds)
+    ds = format_ufloat_type(ds)
     ds = seperate_from_unumpy(ds)
+
     ds.to_netcdf(path)
 
 
 def format_loaded_dataset(ds: xr.Dataset) -> xr.Dataset:
     """
     Formats the loaded dataset, version 0.0.3
-        -Attributes are unflattened
         -Errors are combined with data as unumpy arrays
         -None strings are restored to None type
+        -Attributes are unflattened
     """
-    ds = unflatten_attrs(ds)
-    ds = string_to_attrs(ds, str(type(None)), lambda s: None)
     ds = combine_to_unumpy(ds)
+    ds = restore_none_type(ds)
+    ds = restore_ufloat_type(ds)
+    ds = unflatten_attrs(ds)
+
     return ds
